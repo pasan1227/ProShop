6d5e82fd5e4d4478c047012fd5ef29686892f9a7
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-09-11 13:21:19 +0530

    header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className='py-3 '>
         <Container>
           <h1>Welcome to ProShop</h1>
         </Container>
