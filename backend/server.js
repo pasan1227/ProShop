f822f6b231f1ad247c0a0a0b5ba25ff8e13a7b09
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-09-18 16:46:04 +0530

    Adding controllers

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
+app.listen(PORT, () =>
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -9,7 +9,6 @@ import {
   Form,
   Button,
   Card,
-  ListGroupItem,
 } from 'react-bootstrap';
 import Message from '../components/Message';
 import { addToCart, removeFromCart } from '../actions/cartActions';
