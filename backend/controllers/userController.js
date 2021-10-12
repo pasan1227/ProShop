c787f5863bf98d1c1043344055e433a1db19dcb2
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-10-12 23:07:56 +0530

    Admin User Delete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -105,11 +105,33 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 });
 
 // @desc    Get all users
-// @routes  GET/api/users
+// @routes  GET /api/users
 // @access  Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.json(users);
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
+// @desc    Delete a user
+// @routes  DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.json({ message: 'User removed' });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+};
